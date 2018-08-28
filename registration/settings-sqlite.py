a57e117e515e16ca9f0cf1ba82f5232d88cd03b6
Pavel Anni <panni@redhat.com>
2018-08-28 13:22:29 -0400

    Change to PostgreSQL

--- registration/settings-sqlite.py
+++ registration/settings-sqlite.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'registration.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'studentsdb',
-        'USER': 'django',
-        'PASSWORD': 'django',
-        'HOST': 'postgresql.pa-registration.svc',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
